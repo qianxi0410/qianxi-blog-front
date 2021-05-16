e7089ab74e6dbc182b54dc2128528b3246009a67
qianxi0410 <894871277@qq.com>
2021-05-16 23:56:39 +0800

    :sparkles: add: auto dark

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,14 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  isDarkTheme(): void {
+    const time = new Date().getHours();
+    this.$vuetify.theme.dark = !(time >= 7 && time < 20);
+  }
+
+  mounted(): void {
+    this.isDarkTheme();
+  }
+}
 </script>
