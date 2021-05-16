3d09d0f300c91bdeae91d2709292ccdf65896a98
qianxi0410 <894871277@qq.com>
2021-05-16 23:50:54 +0800

    :sparkles: add: dark link

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,5 +1,9 @@
 <template>
-  <a :href="href" target="_blank">
+  <a
+    :href="href"
+    target="_blank"
+    :class="this.$vuetify.theme.dark ? 'dark' : 'light'"
+  >
     <slot></slot>
   </a>
 </template>
@@ -20,14 +24,13 @@ export default class Link extends Vue {}
 </script>
 
 <style scoped>
-a {
+.light {
   text-decoration: none;
   color: var(--v-accent-base);
   position: relative;
-  margin-left: 0.3em;
-  margin-right: 0.3em;
 }
-a:after {
+
+.light:after {
   content: '';
   position: absolute;
   top: 60%;
@@ -37,8 +40,38 @@ a:after {
   transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
   background-color: var(--v-primary-base);
   opacity: 0.2;
+  margin-right: 0.1rem;
+  margin-left: 0.1rem;
 }
-a:hover:after {
+
+.light:hover:after {
   top: 0%;
+}
+
+.dark {
+  text-decoration: none;
+  color: var(--v-accent-base);
+  position: relative;
+}
+.dark:after {
+  content: '」';
+  color: wheat;
+  opacity: 0.4;
+  transition: all 0.3s ease;
+}
+
+.dark:before {
+  content: '「';
+  color: wheat;
+  opacity: 0.4;
+  transition: all 0.3s ease;
+}
+
+.dark:hover:after {
+  opacity: 1;
+}
+
+.dark:hover:before {
+  opacity: 1;
 }
 </style>
