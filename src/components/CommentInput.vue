a226507207dc694afd6c695a2b0556aecefdf9ea
qianxi0410 <894871277@qq.com>
2021-07-07 18:19:50 +0800

    Update CommentInput

--- src/components/CommentInput.vue
+++ src/components/CommentInput.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="10">
+      <v-col cols="9">
         <v-textarea
           outlined
           placeholder="it is fine to keep silent, dear."
@@ -10,21 +10,36 @@
           clearable
           counter
           clear-icon="mdi-close-circle"
-          color="accent"
+          :color="$vuetify.theme.dark ? 'accent' : 'primary'"
           label="Leave a comment"
         ></v-textarea>
       </v-col>
     </v-row>
     <v-row justify="center" class="mt-n12">
-      <v-col cols="10">
+      <v-col cols="9">
         <v-row justify="space-between">
           <v-col cols="4">
-            <v-text-field label="Nickname" color="accent"></v-text-field>
+            <v-text-field
+              label="Nickname"
+              :color="$vuetify.theme.dark ? 'accent' : 'primary'"
+            ></v-text-field>
           </v-col>
           <v-col cols="2">
-            <v-btn plain class="mt-3" disabled>
-              <v-icon>mdi-share</v-icon>
-            </v-btn>
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  disabled
+                  class="mt-4"
+                  :color="$vuetify.theme.dark ? 'accent' : 'primary'"
+                  plain
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon>mdi-share</v-icon>
+                </v-btn>
+              </template>
+              <span>Share your comment</span>
+            </v-tooltip>
           </v-col>
         </v-row>
       </v-col>
