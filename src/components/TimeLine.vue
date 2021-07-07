08d4b3466b27c00ec194784fe7885cf049a535db
qianxi0410 <894871277@qq.com>
2021-07-07 19:02:40 +0800

    Add TimeLine

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container> </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class TimeLine extends Vue {}
+</script>
