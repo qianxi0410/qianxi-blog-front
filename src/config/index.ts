0197a98e1b4f7aebc02e4fba6031ed847b2989fa
qianxi0410 <894871277@qq.com>
2021-08-23 15:28:55 +0800

    Change banner pic

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,24 +9,24 @@ export const totalVisible = 6;
 
 // index 1 is dark mode img
 export const IndexSrc = [
-  'https://images.wallpaperscraft.com/image/anime_girl_sky_162368_1280x720.jpg',
-  'https://images.wallpaperscraft.com/image/moon_night_silhouettes_202466_1280x720.jpg'
+  'https://w.wallhaven.cc/full/o3/wallhaven-o3lxxl.jpg',
+  'https://w.wallhaven.cc/full/pk/wallhaven-pkjq73.jpg'
 ];
 export const IndexTitle = '千禧';
 export const IndexMotto =
   'An ordinary person, an impure person, an ethical person, a person who has not escaped the low-level tastes, and a somewhat ambitious person.';
 
 export const TimeLineSrc = [
-  'https://images.wallpaperscraft.com/image/girl_anime_hat_182705_1280x720.jpg',
-  'https://images.wallpaperscraft.com/image/space_black_hole_stars_166138_1280x720.jpg'
+  'https://w.wallhaven.cc/full/dp/wallhaven-dpeg9j.jpg',
+  'https://w.wallhaven.cc/full/v9/wallhaven-v9y2vm.jpg'
 ];
 export const TimeLineTitle = 'TimeLine';
 export const TimeLineMotto =
   'One’s destiny, of course, depends on self-struggle, but one must also consider the historical schedule.';
 
 export const AboutSrc = [
-  'https://images.wallpaperscraft.com/image/anime_girl_hair_blue_eyes_16893_1280x720.jpg',
-  'https://images.wallpaperscraft.com/image/girl_kitten_flower_141058_1280x720.jpg'
+  'https://w.wallhaven.cc/full/72/wallhaven-72weyy.jpg',
+  'https://w.wallhaven.cc/full/57/wallhaven-57kvd3.png'
 ];
 export const AboutTitle = 'About';
 export const AboutMotto = 'U wanna know something about me ?';
